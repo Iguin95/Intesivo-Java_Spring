8efa2d848d1574844bb721c71dd3d1722f7bd1c4
Igor Vinnícius <igorquintao22@gmail.com>
2024-11-12 20:29:59 -0300

    Projeto criado

---
+++ src/main/java/com/devjunior/projetoIntensivao/ProjetoIntensivaoApplication.java
@@ -0,0 +1,13 @@
+package com.devjunior.projetoIntensivao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntensivaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntensivaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjunior/projetoIntensivao/ProjetoIntensivaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjunior.projetoIntensivao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntensivaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
